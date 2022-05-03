c8f183c410202fc16165b9c4e0272996df28347c
coebooker <coebooker@gmail.com>
2022-05-03 12:43:34 -0500

    pushed client from other pc, finished

--- listeningClient.py
+++ listeningClient.py
@@ -4,17 +4,12 @@ def simpleClient(location):
 
     client = socket(AF_INET, SOCK_STREAM)
     client.connect((location, 2025))
-    outgoing = "JOIN chatroom3 Tom"
+    outgoing = "JOIN chatroom3 listener"
     client.send(outgoing.encode('ascii'))
     while True:
         msg = client.recv(1024)
         response = msg.decode('ascii')
         print(response)
-        outgoing = input()
-        client.send(outgoing.encode('ascii'))
-        if outgoing == "exit":
-            break
-
     client.close()
 
 simpleClient("172.16.219.84")
