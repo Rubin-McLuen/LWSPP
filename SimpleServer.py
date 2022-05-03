9846f2599a27c411faf5afe8ae6599219a3dd2f2
Rubin-McLuen <rcmcluen20@coe.edu>
2022-05-03 11:14:41 -0500

    wipe chatrooms on server shutdown

--- SimpleServer.py
+++ SimpleServer.py
@@ -2,6 +2,16 @@ from socket import *
 import threading
 from threading import *
 
+sockets = []
+
+import atexit
+
+def exit_handler():
+    with open("chatrooms.txt", 'w'):
+        print("Wiped chatrooms")
+
+atexit.register(exit_handler)
+
 def simpleServer():
     serversocket = socket(AF_INET, SOCK_STREAM)
     serversocket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
@@ -10,11 +20,14 @@ def simpleServer():
     serversocket.bind((IP_address, 2024))
 
 
+
     while True:
         serversocket.listen(1)
         print("Listening on " + IP_address)
         print("Waiting for connection...")
         clientsocket, addr = serversocket.accept()
+        sockets.append((clientsocket,addr))
+
         try:
             threading.Thread(target=processRequest, args=(clientsocket, addr)).start()
         except:
@@ -31,17 +44,23 @@ def processRequest(clientsocket, addr):
         if '' in chatrooms.keys():
             chatrooms.pop('')
         if chat_room_name not in chatrooms.keys():
-            chatrooms[chat_room_name] = [[user_name, addr]]
+            chatrooms[chat_room_name] = [[user_name, addr[0]]]
             sendMessage("Ok, \t" + chat_room_name + " created", clientsocket)
         else:
-            if user_name in chatrooms[chat_room_name]:
-                sendMessage("Denied \t username not unique", clientsocket)
-                clientsocket.close()
-            elif chat_room_name in chatrooms.keys():
-                sendMessage("Ok \t joined " + chat_room_name, clientsocket)
-                chatrooms[chat_room_name].append([user_name, addr])
+            for user in chatrooms[chat_room_name]:
+                if user_name in user[0]:
+                    sendMessage("Denied \t username not unique", clientsocket)
+                    clientsocket.close()
+                elif chat_room_name in chatrooms.keys():
+                    sendMessage("Ok \t joined " + chat_room_name, clientsocket)
+                    chatrooms[chat_room_name].append([user_name, addr[0]])
         updateChatrooms(chatrooms)
 
+    while True:
+        message = receiveMessage(clientsocket.recv(1024))
+        print("message" + message)
+        if "SEND" in message:
+            send_to_chat(message[5:], chat_room_name)
 
 
 
@@ -79,8 +98,19 @@ def updateChatrooms(chatrooms):
         for room in chatrooms:
             file.write(room + " ")
             for user in chatrooms[room]:
-                file.write(user[0] + "," + user[1][0] + " ")
+                file.write(user[0] + "," + user[1] + " ")
             file.write("\n")
+
+def send_to_chat(message, chat_room_name):
+    rooms = getChatrooms()
+    for user in rooms[chat_room_name]:
+        ip = user[1]
+        global sockets
+        for client in sockets:
+            if ip == client[1]:
+                print("yes")
+                sendMessage(message, client[0])
+                print("Sent " + message + " to " + client[0])
 
 simpleServer()
 
